57bf3e9de49bd74a33cb224f43fbaae0221f9dbd
sriramsvk <43366256+sriramsvk@users.noreply.github.com>
2020-09-12 23:49:26 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:44ee57235c92152796646c63674e774c03d48331c4550a51597ac40d7aef4b6d
-size 716
+import sys
+import os
+from API import create_app
+app = create_app()
+
+'''running from Python'''
+''' __name__ is default function that python when executing a code 
+__main__ is default that will called. 
+Below condition is to check program is called directly instead of import 
+import numpy will execute numpy from another code'''
+
+''' pip install flask-restful install the api required 
+for buliting Flask REST API'''
+
+if __name__ == '__main__':
+    ## setting directory to current script directory ##
+    abspath = os.path.abspath(sys.argv[0])  # script path
+    dname = os.path.dirname(abspath)  # get directory name
+    os.chdir(dname)  # change path to script directory
+    app.run(debug=True)
