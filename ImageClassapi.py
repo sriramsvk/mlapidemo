dedf713d83ee8ac0040c1c35624515c5f1cd307b
sriramsvk <43366256+sriramsvk@users.noreply.github.com>
2020-09-13 12:50:41 +0530

    Add files via upload

---
+++ ImageClassapi.py
@@ -0,0 +1,68 @@
+# Resource indicates method that can be access via http methods for performing tasks or retriving information
+# Resource will be defined as Class and within the class, different task to performed by each method
+# Resource will be linked to END Points or URI. Refer init file
+from flask_restful import Resource,reqparse
+import numpy as np
+import base64 # convert Image as Base 64
+import cv2
+
+### Class for Prediction ###
+
+def ImageClass(i):
+   PageType = {
+       1 : 'HCFA',
+       2 : 'UB',
+       3: 'DENTAL',
+       4: 'HCFASUPER',
+       5: 'UBSUPER',
+       6: 'MEDICARE',
+       7: 'COB',
+       8: 'ATTACH',
+       9: 'BLANK',
+       10: 'EOB',
+       11: 'MEOB'
+   }
+   return PageType.get(i)
+
+class ImagePredictClass(Resource):
+    def __init__(self):
+      	# Create a request parser
+        parser = reqparse.RequestParser()
+        #parser.add_argument("image", type=werkzeug.datastructures.FileStorage, help="Base64 encoded image string", required=True, location='files')
+        # help defined the field level defintion validation. error can be listed as dictionary
+        parser.add_argument("image", type=str,help="JPEG Base64 encoded image string", required=True, location='json')
+        self.req_parser = parser
+    def post(self):
+        img = self.req_parser.parse_args(strict=True).get("image",None)
+        if img:
+            # reading image received in post
+            # during request base64 file is converted as utf-8 decode string.
+            # this is reversed when reading file for prediction
+            img = img.encode("utf-8") # encode the string to base64.
+            img64_decode = base64.decodebytes(img)
+            ''' 
+            img64_encode = base64.encodebytes(img)
+            encoded_string = img64_encode.decode("utf-8")
+            
+            '''
+            decode_img = open('predict_decode.jpg', 'wb')  # write file
+            decode_img.write(img64_decode)
+            decode_img.close()
+            ##### Prediction ####
+            from keras.models import load_model
+            #import keras.backend.tensorflow_backend as tb
+            #tb._SYMBOLIC_SCOPE.value=True
+            imgmodel = load_model('\\API\\imageclasscnn')  # loading saved model
+            imgpath = "predict_decode.jpg"
+            input_img = cv2.imread(imgpath, 0)
+            resize_img = cv2.resize(input_img, (100, 100))
+            reshape_img = resize_img.reshape(100, 100, 1)
+            predict_img = np.array(reshape_img)
+            predict_img = predict_img / 255
+            predict_img = predict_img.reshape(1, 100, 100, 1)
+            imclass = imgmodel.predict(predict_img)
+            page_type=ImageClass(np.argmax(imclass))
+            predict_accuarcy=str(round(float(np.max(imclass)*100),2))+"%"
+            return {'Page Type':page_type,'Prediction Accuarcy':predict_accuarcy ,'status':'success'},201
+        else:
+            return {'status':'Invalid Request'},400
---
+++ utreviewapi.py
@@ -0,0 +1,63 @@
+# Resource indicates method that can be access via http methods for performing tasks or retriving information
+# Resource will be defined as Class and within the class, different task to performed by each method
+# Resource will be linked to END Points or URI. Refer init file
+from flask_restful import Resource,reqparse
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import re
+import numpy as np
+REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;:]')  # re.complie is method combine list of value you search in a string
+BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
+MAX_SEQUENCE_LENGTH = 250
+MAX_NB_WORDS = 50000
+def clean_text(text):
+    """
+        text: a string
+        return: modified initial string
+    """
+    text = text.lower()  # lowercase text
+    text = REPLACE_BY_SPACE_RE.sub(' ',
+                                   text)  # replace REPLACE_BY_SPACE_RE symbols by space in text. substitute the matched string in REPLACE_BY_SPACE_RE with space.
+    text = BAD_SYMBOLS_RE.sub('',
+                              text)  # remove symbols which are in BAD_SYMBOLS_RE from text. substitute the matched string in BAD_SYMBOLS_RE with nothing.
+    text = text.replace('x', '')  # replace value with new one. in this example replace "x" with space
+    #    text = re.sub(r'\W+', '', text)
+    text = ' '.join(word for word in text.split() if word not in STOPWORDS)  # remove stopwors from text
+    return text
+
+class UTreviewpredict(Resource):
+    def __init__(self):
+      	# Create a request parser
+        parser = reqparse.RequestParser()
+       # help defined the field level defintion validation. error can be listed as dictionary
+        parser.add_argument("Report Finding", type=str,help="Details of  Patient case history ", required=True, location='json')
+        self.req_parser = parser
+
+    def post(self):
+        finding = self.req_parser.parse_args(strict=True).get("Report Finding", None)
+        if finding:
+            ''' pre processing '''
+            x_pred = finding
+            #x_pred = clean_text(x_pred)
+            #print(x_pred)
+            from keras.preprocessing.text import Tokenizer
+            from keras.preprocessing.sequence import pad_sequences
+            tokenizer = Tokenizer(num_words=MAX_NB_WORDS, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~',
+                                  lower=True)  # assign words or text to numeric index
+
+            x_pred = tokenizer.texts_to_sequences(x_pred)
+            x_pred = pad_sequences(x_pred, maxlen=MAX_SEQUENCE_LENGTH)
+
+            ##### Prediction ####
+            from keras.models import load_model
+            lstm_model = load_model('\\API\\utreview-lstmmodel.h5')
+            y_pred = lstm_model.predict_proba(x_pred)
+            y_predclass = lstm_model.predict_classes(x_pred)
+            labels = ['Overturned Decision of Health Plan', 'Upheld Decision of Health Plan']
+            decision = labels[np.argmax(y_predclass)]
+            print(np.max(y_pred))
+            accuarcy = str(round(float(np.max(y_pred)*100),2)) + "%"
+
+            return {'Review status':  decision, 'Prediction Accuarcy':accuarcy , 'status': 'success'}, 201
+        else:
+            return {'status': 'Invalid Request'}, 400
